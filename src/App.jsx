d90ca54874b791b5238458ab3f423bdf8fb1db1b
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-07-01 00:00:48 +0530

    cursor update

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,29 @@ import AnimatedCursor from "react-animated-cursor";
 function App() {
   return (
     <>
-      <AnimatedCursor
-        innerSize={15}
-        outerSize={25}
-        color="168, 85, 247"
-        outerAlpha={0.4}
-        innerScale={0.7}
-        outerScale={5}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-        ]}
-      />
+      {window.innerWidth > 700 && (
+        <AnimatedCursor
+          innerSize={15}
+          outerSize={25}
+          color="168, 85, 247"
+          outerAlpha={0.4}
+          innerScale={0.7}
+          outerScale={5}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+          ]}
+        />
+      )}
       <Portfolio />
     </>
   );
